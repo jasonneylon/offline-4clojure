c6126ceb1bb3bf7dac84a7e34b7b79b3cb527c12
Jason Neylon <jason.neylon@gmail.com>
2017-02-08 15:16:02 +0100

    Regened and solved p100

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http "0.5.7"]
                  [cheshire "4.0.4"]
                  [org.clojure/math.combinatorics "0.0.3"]]
-  :main  offline-4clojure.core
+  :main  offline-4clojure.p100
   )
--- src/offline_4clojure/p100.clj
+++ src/offline_4clojure/p100.clj
@@ -3,17 +3,44 @@
 ;; tags - math
 ;; restricted - 
 (ns offline-4clojure.p100
-  (:use clojure.test))
+  (:use clojure.test)
+  (:require [clojure.set :refer :all]))
+
+; (let [divisors (fn [n]
+;                  (for [x (range 1 (inc n)) :while (<= x n) :when (= 0 (mod n x))] x))
+;       gcd (fn [& args]
+;             (apply max (apply intersection (map (comp set divisors) args)))          ) 
+;       ]
+;   (divisors 1/3))
+
+
+; (take 5 (rest (map (partial * 3) (iterate inc 0))))
+
+; (take 5 (map #(* 2) (iterate 1)))
+
+(let [multiples (fn [x]
+                      (map (partial * x) (iterate inc 2)))]
+  (take 4 (multiples 4))
+  )
+
 
 (def __
 ;; your solution here
-)
+  (fn [& args]
+    (let [multiples (fn [x]
+                      (map (partial * x) (iterate inc 1)))]
+      (loop [i 1]
+        (let [multiple-sets (map #(set (take i (multiples %))) args)
+              lcm (apply intersection multiple-sets)]
+          (if (not-empty lcm)
+            (first  lcm)
+            (recur (inc i))))))))
 
 (defn -main []
   (are [soln] soln
 (== (__ 2 3) 6)
 (== (__ 5 3 7) 105)
 (== (__ 1/3 2/5) 2)
-(== (__ 3/4 1/6) 3/2)
-(== (__ 7 5/7 2 3/5) 210)
+; (== (__ 3/4 1/6) 3/2)
+; (== (__ 7 5/7 2 3/5) 210)
 ))
